d4840d4d062a94b104450d9f5062d61da35c7e53
sowmya9815 <sowmyaeedala9815@gmail.com>
2022-10-08 23:50:26 +0530

    encap working

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String args[]){
+      RWOnly obj = new RWOnly();
+//      obj.name = "Sowmya";
+//      'name' has private access in 'com.driver.RWOnly'
+      obj.setName("Sowmya");
+      System.out.println(obj.getName());
+  }
 }
